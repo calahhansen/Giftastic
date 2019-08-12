34d75c842bd5077f010c572bae8263b872dbd55d
Calah Hansen <calah.hansen@gmail.com>
2019-08-11 21:16:04 -0500

    form and button set-up with api key and query url fetch and promise

---
+++ assets/app.js
@@ -0,0 +1,28 @@
+ // This .on("click") function will trigger the AJAX Call
+ document.getElementById("find-gif").addEventListener("click", function (event) {
+
+    // Preventing the submit button from trying to submit the form
+    // We're optionally using a form so the user may hit Enter to search instead of clicking the button
+    event.preventDefault();
+
+    // Here we grab the text from the input box
+    const gif = document.getElementById("gif-input").value;
+
+    // API key for Giftastic App on Giphy Developer Dashboard
+    const apikey="z2LQNc66PGXSv6YHaNJ10LFBEPMOSq7N";
+
+    // construct URL
+    const queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=" + apikey + "&limit=5";
+
+    // multi-task with fetch promise
+
+    fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (responseJson) {
+            document.getElementById("gif-view").innerHTML = JSON.stringify(responseJson);
+            console.log(responseJson);
+            console.log(responseJson.runtime);
+        });
+});
