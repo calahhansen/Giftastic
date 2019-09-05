1a7de83c3d81c52ba90c0e3b729bde309d2c59a1
Calah Hansen <calah.hansen@gmail.com>
2019-09-04 20:03:00 -0500

    attempts at fixing homework-ask bowie

--- assets/app.js
+++ assets/app.js
@@ -15,7 +15,7 @@ function displayTopicInfo() {
 
     // construct URL
     const queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apikey + "&q=" + topic + "&limit=" + gifslimit + "&offset=0&rating=" + gifsratings + "&lang=en";
-console.log("queryURL", queryURL);
+    console.log("queryURL", queryURL);
     // const queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifs + "&api_key=z2LQNc66PGXSv6YHaNJ10LFBEPMOSq7N&limit=10"; tutor URL
     // multi-task with fetch promise
 
@@ -40,7 +40,7 @@ console.log("queryURL", queryURL);
                 ratingEl.classList.add("rating");
 
                 // console.log('ratingEl', ratingEl);
-                
+
                 // Display rating
                 ratingEl.innerHTML = "Rating: " + rating;
                 topicDiv.append(ratingEl);
@@ -62,9 +62,10 @@ console.log("queryURL", queryURL);
                 const animateUrl = results[i].images.fixed_height.url;
                 still.setAttribute("data-state", "still");
                 //<img src ="hfjkgdhfghvbhm" data-state="still" />
-                still.setAttribute("class", "img-gif");
+                still.setAttribute("id", "img-gif");
+                // still.setAttribute("onclick", mFunction());
                 //<img src ="hfjkgdhfghvbhm" data-state="still" class="img-gif" onclick=changeAnimate() />
-                still.setAttribute('onclick', 'changeAnimate()');
+                // still.setAttribute('onclick', 'changeAnimate()');
                 still.setAttribute("data-animate", animateUrl);
                 still.setAttribute("data-still", stillURL);
                 // Appending the image
@@ -73,66 +74,85 @@ console.log("queryURL", queryURL);
                 document.getElementById("topics-view").prepend(topicDiv);
             }
 
-                // Tutor did this part and not totally sure what she was doing - unsure of why i needed a custom element from the window?? Couldn't get the code to work...
-                //<img src="jkhfjkbfmjb", data-still = "jhhjbm"></img>
-                // let customElementRegistry = window.customElements;
-                // console.log(customElementRegistry);
-                // customElementRegistry.define("gif-image", MyCustomElement);
-                // customElementRegistry gifImage = document.createElement("img");
-                // gifImage.setAttribute("src", animated);
-                // gifImage.setAttribute("data-still", still);
-                // gifImage.setAttribute("data-animate", animated);
-                // gifImage.setAttribute("data-state", "animate" );
-                // gifImage.setAttribute("class", "gify-img");
-                // gifDiv.append(ratingEl);
-                // gifDiv.append(gifImage);
-                // document.getElementById("gif-view").append(gifDiv);
+            // Tutor did this part and not totally sure what she was doing - unsure of why i needed a custom element from the window?? Couldn't get the code to work...
+            //<img src="jkhfjkbfmjb", data-still = "jhhjbm"></img>
+            // let customElementRegistry = window.customElements;
+            // console.log(customElementRegistry);
+            // customElementRegistry.define("gif-image", MyCustomElement);
+            // customElementRegistry gifImage = document.createElement("img");
+            // gifImage.setAttribute("src", animated);
+            // gifImage.setAttribute("data-still", still);
+            // gifImage.setAttribute("data-animate", animated);
+            // gifImage.setAttribute("data-state", "animate" );
+            // gifImage.setAttribute("class", "gify-img");
+            // gifDiv.append(ratingEl);
+            // gifDiv.append(gifImage);
+            // document.getElementById("gif-view").append(gifDiv);
 
-            });
-        }
-        function changeAnimate(){
-            console.log("work on animating the gif");
-            document.getElementsByClassName("img-gif") = "animateURL";
+        });
+}
+// function changeAnimate() {
+//     console.log("work on animating the gif");
+//     element.getElementsByClassName("img-gif") = "animateURL";
 
-        }
-        
-        // Function for displaying topic data
-        function renderButtons() {
-            // Deleting the buttons prior to adding new topic
-            // (this is necessary otherwise you will have repeat buttons)
-            document.getElementById("buttons-view").innerHTML = "";
+// }
 
-            // Looping through the array of topics
-            for (let i = 0; i < topics.length; i++) {
-                // Then dynamically generating buttons for each topic in the array
-                const a = document.createElement("button");
-                // Adding a class of topic to our button
-                a.classList.add("topic");
-                // Adding a data-attribute
-                a.setAttribute("data-name", topics[i]);
-                // Providing the initial button text
-                a.innerHTML = topics[i];
-                // Adding the button to the buttons-view div
-                document.getElementById("buttons-view").append(a);
-                // Function for displaying the gif info
-                a.addEventListener("click", displayTopicInfo);
-            }
-        }
+// Function for displaying topic data
+function renderButtons() {
+    // Deleting the buttons prior to adding new topic
+    // (this is necessary otherwise you will have repeat buttons)
+    document.getElementById("buttons-view").innerHTML = "";
 
-        /// This function handles events where one button is clicked. keep getting error message....tried switching "add-gif" to "find-gif" and it works! the buttons now show up!
-        document.getElementById("find-topic").addEventListener("click", function (event) {
-            event.preventDefault();
+    // Looping through the array of topics
+    for (let i = 0; i < topics.length; i++) {
+        // Then dynamically generating buttons for each topic in the array
+        const a = document.createElement("button");
+        // Adding a class of topic to our button
+        a.classList.add("topic");
+        // Adding a data-attribute
+        a.setAttribute("data-name", topics[i]);
+        // Providing the initial button text
+        a.innerHTML = topics[i];
+        // Adding the button to the buttons-view div
+        document.getElementById("buttons-view").append(a);
+        // Function for displaying the gif info
+        a.addEventListener("click", displayTopicInfo);
+    }
+}
 
-            // This line grabs the input from the textbox
-            const topic = document.getElementById("topic-input").value.trim();
+/// This function handles events where one button is clicked. keep getting error message....tried switching "add-gif" to "find-gif" and it works! the buttons now show up!
+document.getElementById("find-topic").addEventListener("click", function (event) {
+    event.preventDefault();
 
-            // Adding the gif from the textbox to our array
-            topics.push(topic);
-            console.log(topics);
+    // This line grabs the input from the textbox
+    const topic = document.getElementById("topic-input").value.trim();
+
+    // Adding the gif from the textbox to our array
+    topics.push(topic);
+    console.log(topics);
 
     // Calling renderButtons which handles the processing of our gif array
     renderButtons();
 });
+// document.getElementById("img-gif").addEventListener('click', function (e) {
+//     console.log("on click");
+//     var state = e.getAttribute("data-state");
+// });
+window.onload = function () {
+    var el = document.getElementById('img-gif');
+    console.log("el: " + el);
+    if (el) {
+        el.addEventListener('click', changeAnimate);
+    }
+    else {
+        console.log("not having anything");
+    }
+}
+function changeAnimate() {
+    // var state = e.getAttribute('data-state');
+    // console.log("state: "+state);
+    console.log("animate gif ");
+}
 // Calling the renderButtons function to display the intial buttons
 renderButtons();
 
